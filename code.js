48fa438624b5165b08e075e03fc66040b1afa49f
IamLAM <ingeniero@luismonge.com.mx>
2019-04-12 14:38:10 -0700

    Object created

--- code.js
+++ code.js
@@ -1,1 +1,11 @@
 //In this course you will learn  to develop simple codes with JS oriented object programming
+
+let animals = {
+
+    name: "vertebrados",
+    number: 210
+
+
+}
+
+console.log(animals);
